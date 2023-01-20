c8a977ef2e5e0d1ac1c8ba7060f60f95e5c3091b
SALAH <89524528+sam2043@users.noreply.github.com>
2023-01-21 00:13:35 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oginpage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void closebutton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonlogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (chek.IsChecked == false)
+            {
+                MessageBox.Show("please tcheck");
+            }
+            if (gmail1.Text == "salah@gmail.com" && password1.Text == "salah2004")
+            {
+                MessageBox.Show("Your account is found........");
+            }
+            else
+            {
+                MessageBox.Show("Sorry your account not found.....");
+            }
+
+        }
+        
+    }
+}
